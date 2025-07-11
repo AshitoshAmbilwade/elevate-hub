7892c73423e74d708f6494be90d7700f02f92114
AshitoshAmbilwade <ashitoshambilwade1289@gmail.com>
2025-07-11 20:54:14 +0530

    mongo done backend test api  testing done

--- backend/config/db.js
+++ backend/config/db.js
@@ -12,4 +12,25 @@ const connectDB = async () => {
   }
 };
 
+// Track connection events (for learning and logging) for monitoring better debugging
+//
+mongoose.connection.on("connected", () => {
+  console.log("🔗 Mongoose connected to DB");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(`❌ Mongoose connection error: ${err}`);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("⚠️ Mongoose disconnected from DB");
+});
+
+// Optional: graceful shutdown for Ctrl+C
+process.on("SIGINT", async () => {
+  await mongoose.connection.close();
+  console.log("❎ Mongoose connection closed due to app termination");
+  process.exit(0);
+});
+
 export default connectDB;
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,27 @@
-import app from './app.js';
-
-// Start server
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+const app = express()
 const PORT = process.env.PORT || 5000;
+
+// Load env variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.send('✅ Backend is running!')
+})
+
 app.listen(PORT, () => {
-  console.log(`🚀 Server started on http://localhost:${PORT}`);
-});
-
-
+  console.log(`Example app listening on port ${PORT}`)
+})
