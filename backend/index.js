dc86deadf1a99c62cc436163240f23a1e6b22276
AshitoshAmbilwade <ashitoshambilwade1289@gmail.com>
2025-07-11 20:55:14 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
@@ -16,6 +15,7 @@ connectDB();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
